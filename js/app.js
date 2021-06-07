90efe07528606c3d1d200e788297b63698a44253
marahmusleh <marahaltarefe18@gmail.com>
2021-06-07 17:11:36 +0300

    add a chart

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+let container =document.getElementById('first');
 let leftImage = document.getElementById('left-image');
 let middleImage = document.getElementById('middle-image');
 let rightImage = document.getElementById('right-image');
@@ -10,6 +10,9 @@ let middleIndex;
 let rightIndex;
 
 let countsClick = 0;
+let arrFav=[];
+let arrSeen=[];
+let arrName=[];
 
 
 function Product(name , pathOfImg ){
@@ -18,6 +21,8 @@ function Product(name , pathOfImg ){
     this.seen=0;
     this.fav=0;
     Product.allProducts.push(this);
+    arrName.push(this.name);
+
 }
 Product.allProducts=[];
 
@@ -43,27 +48,32 @@ new Product('usb', './img/usb.gif');
 new Product('water-can', './img/water-can.jpg');
 new Product('wine-glass', './img/wine-glass.jpg');
 
+let lastSeen = [];
 
 function displayThreeImgs (){
     leftIndex=generateRandomIndex();
     middleIndex=generateRandomIndex();
     rightIndex=generateRandomIndex();
 
-    while(leftIndex===rightIndex || rightIndex===middleIndex || leftIndex===middleIndex){
+    while(leftIndex===rightIndex || rightIndex===middleIndex || leftIndex===middleIndex || lastSeen.includes(leftIndex) || lastSeen.includes(middleIndex) || lastSeen.includes(rightIndex)){
         leftIndex=generateRandomIndex();
         rightIndex=generateRandomIndex();
+        middleIndex=generateRandomIndex();
     }
+
+    lastSeen[0]=leftIndex;
+    lastSeen[1]=middleIndex;
+    lastSeen[2]=rightIndex;
+
+
+    
 
     leftImage.src=Product.allProducts[leftIndex].pathOfImg;
-    middleImage.src=Product.allProducts[middleIndex].pathOfImg;
-    rightImage.src=Product.allProducts[rightIndex].pathOfImg;
-
-
-    if(countsClick <= round){
     Product.allProducts[leftIndex].seen++;
+    middleImage.src=Product.allProducts[middleIndex].pathOfImg;
     Product.allProducts[middleIndex].seen++;
+    rightImage.src=Product.allProducts[rightIndex].pathOfImg;
     Product.allProducts[rightIndex].seen++;
-    }
   
 }
 displayThreeImgs();
@@ -73,10 +83,8 @@ function generateRandomIndex(){
     return randomIndex;
 }
 
-
-leftImage.addEventListener('click',handleclicking);
-middleImage.addEventListener('click',handleclicking);
-rightImage.addEventListener('click',handleclicking);
+first.addEventListener('click',handleclicking);
+let btn;
 
 function handleclicking(event){
     countsClick++;
@@ -87,29 +95,60 @@ function handleclicking(event){
         Product.allProducts[middleIndex].fav++;
     }else if(event.target.id==='right-image'){
         Product.allProducts[rightIndex].fav++;
+    }else{
+        alert('You should click on the image');
+        countsClick--;
     }
     displayThreeImgs();
 }else {
-    console.log(Product.allProducts);
-    gettingList();
-    leftImage.removeEventListener('click',handleclicking);
-    middleImage.removeEventListener('ckick',handleclicking);
-    rightImage.removeEventListener('click',handleclicking);
-
+    btn = document.getElementById('button');
+    btn.addEventListener('click',handleShowing);
+    first.removeEventListener('click',handleclicking);
     }
-}
+    }
+
+    
+function handleShowing(){
+    gettingList();
+    gettingChart();
+    btn.removeEventListener('click',handleShowing);
+  }
 
 
-document.getElementById('button').onclick =function gettingList(){
+
+    function gettingList(){
     let ul = document.getElementById('third');
     for(let i = 0 ; i <Product.allProducts.length; i++ ){
+        arrFav.push(Product.allProducts[i].fav);
+        arrSeen.push(Product.allProducts[i].seen);
       let li = document.createElement('li');
       ul.appendChild(li);
       li.textContent = `${Product.allProducts[i].name} had ${Product.allProducts[i].fav} Votes , and was seen ${Product.allProducts[i].seen} Times. `;
     }
-  
   }
   
+  function gettingChart(){
 
+  let chartEl = document.getElementById('chart');
+  let chart = new Chart(chartEl, {
+      type : 'bar',
+      data : {
+          labels: arrName,
+          datasets: [{
+              label:'Total of Votes',
+              data : arrFav,
+              backgroundColor: [
+                'rgba(255, 76, 132, 0.4)',
+              ],
+          },{
+            label: 'Number of viewed products',
+            data: arrSeen,
+            backgroundColor: [
+                'rgba(200, 120, 132, 0.5)',
+            ],
+          }
 
-
+          ]
+      },
+  }); 
+}
